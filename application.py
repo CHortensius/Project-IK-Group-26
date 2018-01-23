1c645d9ce1e94527211dcbed1b2bbb5314c580fb
Alex Zwijnenburg <alexzwijnenburg@ymail.com>
2018-01-23 15:20:53 +0000

    uploads fixed

--- application.py
+++ application.py
@@ -6,10 +6,15 @@ from tempfile import mkdtemp
 
 from helpers import *
 
+import shutil
 import time
+import os
 
 # configure application
 app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.basename('/uploadedimgs/')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # ensure responses aren't cached
 if app.config["DEBUG"]:
@@ -37,106 +42,20 @@ def index():
 
     return render_template("index.html")
 
-@app.route("/upload")
-@login_required
+@app.route("/upload", methods=["GET", "POST"])
 def upload():
     if request.method == "POST":
 
-        <?php
-        $db = mysqli_connect("localhost", "root", "", "image_upload");
+        file = request.files["image"]
+        f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 
-        $msg = "";
+        # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
+        file.save(f)
 
-        if (isset($_POST['upload'])) {
-  	        $image = $_FILES['image']['name'];
-  	        $image_text = mysqli_real_escape_string($db, $_POST['image_text']);
-
-  	        $target = "images/".basename($image);
-
-  	        $sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
-  	        mysqli_query($db, $sql);
-
-  	    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-  		    $msg = "Image uploaded successfully";
-  	    }else{
-  		    $msg = "Failed to upload image";
-  	    }
-    }
-    $result = mysqli_query($db, "SELECT * FROM images");
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<title>Upload your masterpiece here!</title>
-<style type="text/css">
-   #content{
-   	width: 50%;
-   	margin: 20px auto;
-   	border: 1px solid #cbcbcb;
-   }
-   form{
-   	width: 50%;
-   	margin: 20px auto;
-   }
-   form div{
-   	margin-top: 5px;
-   }
-   #img_div{
-   	width: 80%;
-   	padding: 5px;
-   	margin: 15px auto;
-   	border: 1px solid #cbcbcb;
-   }
-   #img_div:after{
-   	content: "";
-   	display: block;
-   	clear: both;
-   }
-   img{
-   	float: left;
-   	margin: 5px;
-   	width: 300px;
-   	height: 140px;
-   }
-</style>
-</head>
-<body>
-<div id="content">
-
-
-  <?php
-    while ($row = mysqli_fetch_array($result)) {
-      echo "<div id='img_div'>";
-      	echo "<img src='images/".$row['image']."' >";
-      	echo "<p>".$row['image_text']."</p>";
-      echo "</div>";
-    }
-  ?>
-
-  <form method="POST" action="index.php" enctype="multipart/form-data">
-  	<input type="hidden" name="size" value="1000000">
-  	<div>
-  	  <input type="file" name="image">
-  	</div>
-  	<div>
-      <textarea
-      	id="text"
-      	cols="40"
-      	rows="4"
-      	name="image_text"
-      	placeholder="Say something about this image..."></textarea>
-  	</div>
-  	<div>
-  		<button type="submit" name="upload">POST</button>
-  	</div>
-  </form>
-</div>
-</body>
-</html>
+        return render_template('upload.html')
 
     else:
-        return render_template("newpass.html")
+        return render_template("upload.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -221,42 +140,5 @@ def register():
 
     else:
         return render_template("register.html")
-
-@app.route("/newpass", methods=["GET", "POST"])
-@login_required
-def newpass():
-    """Laat de gebruiker zijn wachtwoord veranderen"""
-
-    if request.method == "POST":
-
-        # controleert of de gebruiker alle velden ingevuld heeft
-        if not request.form.get("password"):
-            return apology("must provide password")
-
-        elif not request.form.get("newpass"):
-            return apology("must provide new password")
-
-        elif not request.form.get("passwordconfirm"):
-            return apology("must provide password confirmation")
-
-        # controleert of de nieuwe wachtwoorden matchen
-        if request.form.get("newpass") != request.form.get("passwordconfirm"):
-            return apology("passwords must match")
-
-        # haalt het huidige wachtwoord van de gebruiker erbij
-        oldpass = db.execute("SELECT hash FROM users WHERE id = :id", id = session["user_id"])
-
-        # controleert of de gebruiker het goede wachtwoord ingevuld heeft
-        if not pwd_context.verify(request.form.get("password"), oldpass[0]["hash"]):
-            return apology("invalid password")
-
-        # update gebruiker's wachtwoord
-        db.execute("UPDATE users SET hash = :hash WHERE id = :id", hash = pwd_context.encrypt(request.form.get("newpass")), id = session["user_id"])
-
-        # redirect user to home page
-        return redirect(url_for("index"))
-
-    else:
-        return render_template("newpass.html")
 
 # Groetjes, Cas, Sooph en Lex

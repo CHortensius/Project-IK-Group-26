1e02b3c84514781f09eae969ba502f87702923d6
Cas Hortensius <cas.hortensius@macato.nl>
2018-01-24 11:25:08 +0000

    comments toegevoegd

--- ImgurApi/test.py
+++ ImgurApi/test.py
@@ -8,3 +8,10 @@ access_token= 'f8abdffaf2902a85d6ebb44af4f4d2c010d095bd'
 client = ImgurClient(client_id, client_secret, access_token, refresh_token)
 image = client.upload_from_path('photo1.png', anon=True)
 print(image)
+
+
+
+## Alex, dit bestand zorgt ervoor dat de, lokaal opgeslagen afbeeldingen, worden geupload naar de imgur website.
+## Wanneer je deze code runt krijg je een dict eruit, waar verschillende keys staan. De laatste key is een url naar waar de foto staat. Als je deze in de database zet,
+## kunnen wij m straks makkelijk laten zien op de website. Dan is het namelijk alleen de url aanroepen. Waar nu photo1.png staat, moet dan het pad naar waar de foto is opgeslagen.
+## Verder zijn de andere bestanden in de ImgurApi map niet echt van toepassing, maar laat ik voor de zekerheid even staan. Hoop dat je dit verhaal begrijpt :)

f042f004e96bf47cdb280ab173c8abcebbda8973
Cas Hortensius <cas.hortensius@macato.nl>
2018-01-31 20:55:59 +0000

    repository opgeschoond

--- updategif.py
+++
@@ -1,4 +0,0 @@
-def updategif(gif_url):
-    db.execute("UPDATE Accounts SET profilegif = :profilegif WHERE id = :id", profilegif = gif_url, id = session["user_id"])
-
-    return redirect(url_for("profielpagina"))
